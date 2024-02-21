0174953fe2d5c22d160177684774635954734760
Himanshu Mishra <sai.himanshum011@gmail.com>
2024-02-21 12:59:19 +0530

    Provide the redux store to the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
+import { Provider } from 'react-redux';
+
 import Header from './components/Header.tsx';
 import Shop from './components/Shop.tsx';
 import Product from './components/Product.tsx';
 import { DUMMY_PRODUCTS } from './dummy-products.ts';
+import { store } from './store/store.ts';
+
+/*  just as with the context provider component this provider component, which is provided by that third party library, react-redux, should be wrapped around all components that need to change the data in the Redux store or that want to read the data. */
 
 function App() {
   return (
-    <>
+    <Provider store={store}>
       <Header />
       <Shop>
         {DUMMY_PRODUCTS.map((product) => (
@@ -14,7 +19,7 @@ function App() {
           </li>
         ))}
       </Shop>
-    </>
+    </Provider>
   );
 }
 
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,7 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { cartSlice } from './cart-slice';
 
-configureStore({
+export const store = configureStore({
   // This configuration object is where we can define our reducers ( a map of reducers) and middleware
 
   reducer: {
