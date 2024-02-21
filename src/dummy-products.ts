78a9a687f39eaa43df0f0ba29d88a86f2a10623e
Himanshu Mishra <sai.himanshum011@gmail.com>
2024-02-21 12:34:30 +0530

    Set a state type Add a method to randomly generate a GUID

--- src/dummy-products.ts
+++ src/dummy-products.ts
@@ -5,9 +5,15 @@ import merlotSuit from './assets/merlot-suit.jpg';
 import moonlightDress from './assets/moonlight-dress.jpg';
 import denimPioneer from './assets/denim-pioneer.jpg';
 
+export const generateGUID = (): string => {
+  const timestamp = new Date().getTime().toString(16);
+  const randomNum = Math.floor(Math.random() * 1000000).toString(16);
+  return `${timestamp}-${randomNum}`;
+};
+
 export const DUMMY_PRODUCTS = [
   {
-    id: 'p1',
+    id: generateGUID(),
     image: mochaOvercoat,
     title: 'Majestic Vintage Mocha Overcoat',
     price: 129.99,
@@ -15,7 +21,7 @@ export const DUMMY_PRODUCTS = [
       'Channel timeless sophistication with this stunning mocha overcoat. Crafted for the discerning gentleman who appreciates the fine blend of vintage charm and modern elegance.',
   },
   {
-    id: 'p2',
+    id: generateGUID(),
     image: dreamGown,
     title: 'Enchanting Blush Dream Gown',
     price: 189.99,
@@ -24,7 +30,7 @@ export const DUMMY_PRODUCTS = [
   },
 
   {
-    id: 'p3',
+    id: generateGUID(),
     image: rainJacket,
     title: 'Vintage Sunshine Rain Jacket',
     price: 49.99,
@@ -32,7 +38,7 @@ export const DUMMY_PRODUCTS = [
       'Brace the showers in style! Our Vintage Sunshine Rain Jacket ensures that you stand out, even in the dullest weather. Because rain is never a reason to compromise on your fashion quotient.',
   },
   {
-    id: 'p4',
+    id: generateGUID(),
     image: merlotSuit,
     title: 'Classic Merlot Business Suit',
     price: 249.99,
@@ -40,7 +46,7 @@ export const DUMMY_PRODUCTS = [
       'Step into the boardroom with unmatched confidence in our Classic Merlot Business Suit. Exuding an air of refined class and understated power, it is ideal for the modern executive who values tradition and elegance.',
   },
   {
-    id: 'p5',
+    id: generateGUID(),
     image: moonlightDress,
     title: 'Ethereal Moonlight Evening Dress',
     price: 159.99,
@@ -48,7 +54,7 @@ export const DUMMY_PRODUCTS = [
       'Sweep the room off its feet in our Ethereal Moonlight Evening Dress. Crafted to mimic the allure of the moonlight, this dress is a nod to those who appreciate subtle glamour and a standout silhouette.',
   },
   {
-    id: 'p6',
+    id: generateGUID(),
     image: denimPioneer,
     title: 'Pioneer Rugged Denim Jacket',
     price: 79.99,
