18e02b5fa165925d96b55076ff2af8a1b3e6c41c
Himanshu Mishra <sai.himanshum011@gmail.com>
2024-02-21 14:10:04 +0530

    Add and remove items from the cart

--- src/components/CartItems.tsx
+++ src/components/CartItems.tsx
@@ -1,9 +1,30 @@
+import { CartItem, addToCart, removeFromCart } from '../store/cart-slice';
+import { useAppSelector, useAppDispatch } from '../store/hooks';
+
 export default function CartItems() {
+  const dispatch = useAppDispatch();
+  const cartItems = useAppSelector((state) => state.cart.items);
+
+  const totalPrice = cartItems.reduce(
+    (currVal, currItem) => currVal + currItem.price * currItem.quantity,
+    0
+  );
+
+  const formattedTotalPrice = totalPrice.toFixed(2);
+
+  const handleAddToCart = function (item: CartItem) {
+    dispatch(addToCart({ ...item }));
+  };
+
+  const handleRemoveFromCart = function (id: string) {
+    dispatch(removeFromCart(id));
+  };
   return (
     <div id='cart'>
-      <p>No items in cart!</p>
+      {cartItems.length === 0 && <p>No items in cart!</p>}
 
-      {/* <ul id="cart-items">
+      {cartItems.length > 0 && (
+        <ul id='cart-items'>
           {cartItems.map((item) => {
             const formattedPrice = `$${item.price.toFixed(2)}`;
 
@@ -13,7 +34,7 @@ export default function CartItems() {
                   <span>{item.title}</span>
                   <span> ({formattedPrice})</span>
                 </div>
-                <div className="cart-item-actions">
+                <div className='cart-item-actions'>
                   <button onClick={() => handleRemoveFromCart(item.id)}>
                     -
                   </button>
@@ -23,11 +44,12 @@ export default function CartItems() {
               </li>
             );
           })}
-        </ul> */}
+        </ul>
+      )}
 
-      {/* <p id="cart-total-price">
-        Cart Total: <strong>{formattedTotalPrice}</strong>
-      </p> */}
+      <p id='cart-total-price'>
+        Cart Total: <strong>${formattedTotalPrice}</strong>
+      </p>
     </div>
   );
 }
