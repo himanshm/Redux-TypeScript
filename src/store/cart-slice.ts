c7f9ab01e80e4401ab64ace7ede5b605a5fbddf6
Himanshu Mishra <sai.himanshum011@gmail.com>
2024-02-21 12:44:06 +0530

    A First Reducer & Controlling the Action Payload Type

--- src/store/cart-slice.ts
+++ src/store/cart-slice.ts
@@ -1,4 +1,4 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, type PayloadAction } from '@reduxjs/toolkit';
 
 type CartItem = {
   id: string;
@@ -15,11 +15,19 @@ const initialState: CartState = {
   items: [],
 };
 
+type ActionPayload = {
+  id: string;
+  title: string;
+  price: number;
+};
+
+/* the PayloadAction type, which should be imported from Redux toolkit, is the type of action we should assign here and you should use this type whenever you are dealing with an action that carries a payload. So that does not just have a type, but also some extra data attached to it. */
+
 export const cartSlice = createSlice({
   name: 'cart',
   initialState,
   reducers: {
-    addToCart() {},
+    addToCart(state, action: PayloadAction<ActionPayload>) {},
     removeFromCart() {},
   },
 });
